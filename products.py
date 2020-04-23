5accf593e37206e7fcfcb40a3f4fd64fda1658c8
Chen Jui <larstraschen@gmail.com>
2020-04-23 16:15:35 +0800

    encoding

--- products.py
+++ products.py
@@ -8,6 +8,7 @@ while True:
 	products.append([(name), (price)])
 print(products)
 
-with open ('product.csv', 'w') as f:
+with open ('product.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
