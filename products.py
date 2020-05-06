1cf82a73bd0137753a94d1a70debb1f77e79926b
Chen Jui <larstraschen@gmail.com>
2020-05-06 16:48:08 +0800

    'isfile'

--- products.py
+++ products.py
@@ -1,12 +1,22 @@
-#讀取檔案
+import os
 products=[]
-with open('product.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line: #不能空格
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+
+if os.path.isfile('product.csv'):
+	print('恭喜中獎')
+	with open('product.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line: #不能空格
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+
+else:
+	print('找不到檔案....')
+
+#讀取檔案
+
+
 #使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
